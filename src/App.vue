409aa63cef90f32bff9c5754b388d9d52f830c51
Rômulo F. Douro <rfdouro@gmail.com>
2022-03-07 10:36:59 -0300

    Alteração inicial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Início</router-link> |
+    <router-link to="/about">Sobre</router-link>
   </div>
   <router-view/>
 </template>
